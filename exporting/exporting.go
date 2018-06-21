d4b247dbe3c6f1cbad2ac1f43b79c8f86f6513bf
wooiliang <chuawooiliang@gmail.com>
2018-06-21 10:13:20 +0800

    Second day afternoon session.

---
+++ exporting/exporting.go
@@ -0,0 +1,37 @@
+// All material is licensed under the Apache License Version 2.0, January 2004
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Create a package named toy with a single exported struct type named Toy. Add
+// the exported fields Name and Weight. Then add two unexported fields named
+// onHand and sold. Declare a factory function called New to create values of
+// type toy and accept parameters for the exported fields. Then declare methods
+// that return and update values for the unexported fields.
+//
+// Create a program that imports the toy package. Use the New function to create a
+// value of type toy. Then use the methods to set the counts and display the
+// field values of that toy value.
+package main
+
+import (
+	"fmt"
+
+	"github.com/ardanlabs/gotraining/topics/go/language/exporting/exercises/template1/toy"
+)
+
+// Add imports.
+
+func main() {
+
+	// Use the New function from the toy package to create a value of
+	// type toy.
+	t := toy.New("test", 80)
+
+	// Use the methods from the toy value to set some initialize
+	// values.
+	t.UpdateOnHand(10)
+	t.UpdateSold(10)
+
+	// Display each field separately from the toy value.
+	fmt.Println(t.OnHand())
+	fmt.Println(t.Sold())
+}
